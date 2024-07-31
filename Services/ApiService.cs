36e0f7aa9ec8208fd37dee9fc32ef6d4eae3ef88
bassinediallo <151927637+bassinediallo@users.noreply.github.com>
2024-07-31 17:43:36 +0000

    ApiService.cs

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -13,6 +13,7 @@ namespace WPFModernVerticalMenu.Services
     {
         private static readonly HttpClient client = new HttpClient();
 
+        /// permet de lister categorie comme parametre un objet categorie
         public async Task<List<Category>> GetCategories()
         {
             string url = "http://localhost/backend/list_categories.php";
@@ -20,6 +21,7 @@ namespace WPFModernVerticalMenu.Services
             return JsonConvert.DeserializeObject<List<Category>>(response);
         }
 
+        /// permet de lister produit comme parametre un objet produit
         public async Task<List<Product>> GetProducts()
         {
             string url = "http://localhost/backend/list_produits.php";
@@ -27,6 +29,7 @@ namespace WPFModernVerticalMenu.Services
             return JsonConvert.DeserializeObject<List<Product>>(response);
         }
 
+         /// permet d'ajouter une categorie comme parametre un objet categorie
         public async Task AddCategory(Category category)
         {
             string url = "http://localhost/backend/create_category.php";
@@ -35,6 +38,7 @@ namespace WPFModernVerticalMenu.Services
             await client.PostAsync(url, content);
         }
 
+         /// permet de modifier une categorie comme parametre l'id categorie
         public async Task UpdateCategory(Category category)
         {
             string url = "http://localhost/backend/update_category.php";
@@ -43,6 +47,7 @@ namespace WPFModernVerticalMenu.Services
             await client.PutAsync(url, content);
         }
 
+         /// permet de supprimer une categorie comme parametre l'id categorie
         public async Task DeleteCategory(int id)
         {
             string url = "http://localhost/backend/delete_category.php";
@@ -52,6 +57,7 @@ namespace WPFModernVerticalMenu.Services
             await client.PostAsync(url, content);
         }
 
+         /// permet d'ajouter un produit comme parametre un objet produit
         public async Task AddProduct(Product product)
         {
             string url = "http://localhost/backend/create_produit.php"; // Assurez-vous que cette URL est correcte
@@ -64,7 +70,7 @@ namespace WPFModernVerticalMenu.Services
         }
 
 
-
+         /// permet de modifier un produit comme parametre l'id produit
         public async Task UpdateProduct(Product product)
         {
             string url = "http://localhost/backend/update_produit.php"; // Assurez-vous que cette URL est correcte
@@ -76,7 +82,7 @@ namespace WPFModernVerticalMenu.Services
             Console.WriteLine($"Response from server: {responseBody}"); // Log the response for debugging
         }
 
-
+         /// permet de supprimer un produit comme parametre l'id produit
         public async Task DeleteProduct(int id)
         {
             string url = "http://localhost/backend/delete_produit.php";
